0b90a3b1268c0b310b123d80456e0e42b7d95810
aswinsadan <sadanaswin@gmail.com>
2017-03-17 10:48:55 +0530

    fifteenth

---
+++ Employee (8).java
@@ -0,0 +1,33 @@
+
+
+package employee;
+
+
+public class Employee  {
+  int id,salary;
+  String name;
+  public Employee(int id,String name){
+      this.id=id;
+      this.name=name;
+      
+  }
+  
+    public static void main(String[] args) {
+        PermanantEmployee p=new PermanantEmployee(101,"ASWIN", 9);
+        p.setSalary(15000);
+        p.setPf(1000);
+        p.setGratuity(1000);
+        p.calculatesalary();
+        p.SeasonOffer(10000);
+        p.regularoffer(10000);
+        ContractedEmployee c=new ContractedEmployee(102, "AMAL", 6);
+        c.setSalary(10000);
+        c.setFine(4000);
+        c.calculatesalary();
+        c.SeasonOffer(10000);
+        c.regularoffer(10000);
+        
+    }
+
+  
+}
